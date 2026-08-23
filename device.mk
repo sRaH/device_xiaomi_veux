e0698727daf07d845c07cc29755a9648ed1ce291
sRaH <3193012+sRaH@users.noreply.github.com>
2026-08-23 10:47:38 +0200

    Enable ART userfaultfd GC

--- device.mk
+++ device.mk
@@ -6,6 +6,9 @@
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# Enable ART userfaultfd GC
+PRODUCT_ENABLE_UFFD_GC := true
+
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
